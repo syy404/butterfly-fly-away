2f8d25431c33ebfe69f77a991cd1a8b0687589e6
sy <49295116+syy404@users.noreply.github.com>
2023-05-13 23:13:25 +0800

    test1

---
+++ main-script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  var count = 0;
+  var chatCount = 0;
+  var popCount = 0;
+  var images = [
+      "image/chpt1-1.jpg",
+      "image/chpt1-2.png",
+      "image/chpt1-3.png",
+      "image/chpt1-4.png",
+      "image/chpt1-5.png",
+  ];
+  var imageIndex = 0;
+
+  var chats = $('.chpt1-chat');
+  var pops = $('.chpt1-pop');
+
+  $('.chpt1-container').on('click', function() {
+      // 切换背景图并隐藏元素
+      if(count % 5 == 4) {
+          $('.chpt1-container').css('background-image', 'url(' + images[imageIndex] + ')');
+          chats.slice(0, chatCount).hide();
+          pops.slice(0, popCount).hide();
+          imageIndex += 1;
+      }
+      // 显示chpt1-chat
+      else if(count % 5 < 2) {
+          chats.eq(chatCount).show();
+          chatCount += 1;
+      }
+      // 显示chpt1-pop
+      else if(count % 5 == 2) {
+          pops.eq(popCount).show();
+          popCount += 1;
+      }
+      count += 1;
+  });
+});
