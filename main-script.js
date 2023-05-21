d59cf05bdf05e90dc42a6a7f17174c56e0b21fea
sy <sphysicaly@gmail.com>
2023-05-21 11:29:10 +0800

    add chpt3-pop

--- main-script.js
+++ main-script.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
   ];
   var imageIndex = 0;
 
-  var chats = $(".chpt0-chat");
-  var pops = $(".chpt0-pop");
+  var chats0 = $(".chpt0-chat");
+  var pop0 = $(".chpt0-pop");
 
   $(".chpt0-container").on("click", function () {
     if (count % 5 == 4) {
@@ -20,25 +20,24 @@ $(document).ready(function () {
         "background-image",
         "url(" + images[imageIndex] + ")"
       );
-      chats.slice(0, chatCount).hide();
-      pops.slice(0, popCount).hide();
+      chats0.slice(0, chatCount).hide();
+      pop0.slice(0, popCount).hide();
       if (imageIndex < images.length - 1) {
         imageIndex += 1;
       }
     } else if (count % 5 < 2) {
-      chats.eq(chatCount).fadeIn();
+      chats0.eq(chatCount).fadeIn();
       chatCount += 1;
     } else if (count % 5 == 2) {
-      pops.eq(popCount).fadeIn();
+      pop0.eq(popCount).fadeIn();
       popCount += 1;
     }
     count += 1;
-    if (chatCount >= chats.length && popCount >= pops.length) {
-      $(".container").css({
-        border: "1px solid black", // 你可以根据需要改变这个值
-        opacity: "1", // 使其显示
-        "z-index": "1", // 放在 chpt0-container 上面
-      });
-    }
+  });
+  $(document).ready(function () {
+    $(".chpt3-pop-text").hide();
+    $(".chpt3-popchan").on("click", function () {
+      $(this).siblings(".chpt3-pop-text").slideToggle();
+    });
   });
 });
